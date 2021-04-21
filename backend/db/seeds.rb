4354600e39db87f6590ec7ef3c28be250d3113d1
Sean <Sransonette@gmail.com>
2021-04-21 00:42:55 -0500

    create backend schema

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+pokemon = Pokemon.create(name: 'Bulbasaur', pokemon_type: 'Grass')
