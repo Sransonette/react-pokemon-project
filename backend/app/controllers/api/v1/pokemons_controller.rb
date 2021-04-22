43b5490ea156de62592031008e68a40add580e42
Sean <Sransonette@gmail.com>
2021-04-21 21:41:28 -0500

    setup controller

--- backend/app/controllers/api/v1/pokemons_controller.rb
+++ backend/app/controllers/api/v1/pokemons_controller.rb
@@ -1,3 +1,35 @@
 class Api::V1::PokemonsController < ApplicationController
-    
+
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons
+
+    end
+
+    def show
+        pokemon = Pokemon.find_by(params[:id])
+        render json: pokemon
+
+    end
+
+    def create
+        pokemon = Pokemon.create(pokemons_params)
+            if pokemon.save
+                render json: pokemon
+            else
+                render json: {error: 'Card was not added to collection'}
+            end
+    end
+
+    def delete
+        pokemon = Pokemon.find_by(params[:id])
+        pokemon.destroy
+    end
+
+    private
+
+    def pokemons_params
+        params.require(:pokemons).permit(:name, :pokemon_type)
+    end
+
 end
