90e6a12f235418c815aecd4bb5acf7b85a032421
Sean <Sransonette@gmail.com>
2021-05-11 09:47:24 -0500

    remove unneeded code

--- frontend/src/components/PokemonList.js
+++ frontend/src/components/PokemonList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {deletePokemon} from '../actions/deletePokemon'
-import Like from '../components/Like'
 
 
 const PokemonList = (props) => {
@@ -21,6 +20,7 @@ const PokemonList = (props) => {
                     <div className={"trainerPokemonNames"}>
                     <li key={pokemon.id}><Link to={`/pokemon/${pokemon.id}`}>{pokemon.name} </Link>
                     <button onClick={() => handleDelete(pokemon.id)}>Delete</button>
+                    
                     </li>
                     </div>)
                 )
