3912a51d5505b9b287300103743bda537397905f
Sean <Sransonette@gmail.com>
2021-09-11 13:49:31 -0500

    deleted created image folder and put image for background in public folder instead

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 import SearchBar from './components/SearchBar.js';
 
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+
         <Header />
         <NavBar />
         <SearchBar />
