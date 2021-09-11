b9cb79ffe2d7bb98043dfa08f5d12e5464cff8e6
Sean <Sransonette@gmail.com>
2021-09-11 13:40:00 -0500

    add functional component to render background

---
+++ frontend/src/components/Background.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Background = () => {
+    return (
+        <div style={{ backgroundImage: `url("https://www.pinpng.com/picture/hRhoxhR_http-i-imgur-com-sfdvxef-rotom-pokedex-iphone/")` }}>
+
+        </div>
+    );
+}
+
+export default Background;
