5d611bc7410ae99c1eb9275d2b7eeec42d9ff8c1
Sean <Sransonette@gmail.com>
2021-09-24 20:38:35 -0500

    create new file for fetch call to get pokemon sprite images

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,6 @@ const store = createStore(manageStore, compose(applyMiddleware(thunk), window.__
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-
       <App />
     </Router>
   </Provider>,
