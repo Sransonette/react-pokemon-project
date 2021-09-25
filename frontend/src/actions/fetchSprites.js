229bea4e257de28b9d76cef10a8da652bcf2a542
Sean <Sransonette@gmail.com>
2021-09-24 20:54:30 -0500

    install axios to assist in fetch calls for sprites

---
+++ frontend/src/actions/fetchSprites.js
@@ -0,0 +1,16 @@
+// export function fetchPokedex(page) {
+
+//     const perpage = 150;
+
+
+//     return (dispatch) => {
+//         fetch(`https://pokeapi.co/api/v2/pokemon?limit=${perpage}&offset=${0}`)
+//             .then(resp => resp.json())
+//             .then(pokemon => dispatch({
+//                 type: 'FETCH_SPRITE',
+//                 payload: pokemon
+
+//             }))
+//     }
+
+// }
